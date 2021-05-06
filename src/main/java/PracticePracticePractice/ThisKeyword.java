9b83afb92a15e41f9d98bc0d83994c70f25c6c8b
Hana Dawson <stubbornunicorn95@gmail.com>
2021-05-06 11:13:13 -0600

    Practicing This keyword.

--- src/main/java/PracticePracticePractice/ThisKeyword.java
+++ src/main/java/PracticePracticePractice/ThisKeyword.java
@@ -1,7 +1,16 @@
 package PracticePracticePractice;
 
 public class ThisKeyword {
+  int x;
 
+  // This is a constructor
+  public ThisKeyword(int x) {
+    this.x = x;
+  }
 
-
+  // Calling the constructor
+  public static void main(String[] args) {
+    ThisKeyword myObj = new ThisKeyword(5);
+    System.out.println("Value of x = " + myObj.x);
+  }
 }
